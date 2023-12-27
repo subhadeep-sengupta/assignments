ed6836248260675cdbe67ce28f838fcc9614b264
subhadeep-sengupta <www.subhadeepsengupta2002@gmail.com>
2023-12-27 20:07:09 +0530

    1st week done

--- 01-js/medium/countVowels.js
+++ 01-js/medium/countVowels.js
@@ -6,7 +6,21 @@
 */
 
 function countVowels(str) {
-    // Your code here
+  // Your code here
+  const lowercaseArray = str.toLowerCase().split("");
+  let count = 0;
+  for (let i = 0; i < lowercaseArray.length; i++) {
+    if (
+      lowercaseArray[i] == "a" ||
+      lowercaseArray[i] == "e" ||
+      lowercaseArray[i] == "i" ||
+      lowercaseArray[i] == "o" ||
+      lowercaseArray[i] == "u"
+    ) {
+      count++;
+    }
+  }
+  return count;
 }
 
 module.exports = countVowels;
--- 01-js/medium/times.js
+++ 01-js/medium/times.js
@@ -9,5 +9,16 @@ There is no automated test for this one, this is more for you to understand time
 */
 
 function calculateTime(n) {
-    return 0.01;
+  let sum = 0;
+  for (let i = 1; i < n; i++) {
+    sum = sum + i;
+  }
+  return sum;
 }
+const beforeDate = new Date();
+const beforeTimeInMs = beforeDate.getSeconds();
+console.log(beforeTimeInMs);
+calculateTime(1000000000);
+const afterDate = new Date();
+const afterTimeInMs = afterDate.getSeconds();
+console.log(afterTimeInMs);
