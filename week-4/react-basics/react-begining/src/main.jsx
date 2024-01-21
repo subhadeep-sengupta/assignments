8841b8e5a2305f08e930fc9083f3481f1e7c7436
subhadeep-sengupta <www.subhadeepsengupta2002@gmail.com>
2024-01-21 20:16:17 +0530

    week-4

--- week-4/react-basics/react-begining/src/main.jsx
+++ week-4/react-basics/react-begining/src/main.jsx
@@ -9,3 +9,15 @@ ReactDOM.createRoot(document.getElementById("root")).render(
     <App />
   </React.StrictMode>
 );
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import Todo from "./todo.jsx";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <Todo />
+  </React.StrictMode>
+);
