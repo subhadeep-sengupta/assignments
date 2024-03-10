2dab9bd23ec90d0c4862455a379f4bf718801a13
subhadeep-sengupta <www.subhadeepsengupta2002@gmail.com>
2024-03-10 22:28:26 +0530

    Prisma done

--- week-10/2-prisma-simple/src/db/todo.ts
+++ week-10/2-prisma-simple/src/db/todo.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
@@ -12,8 +12,27 @@ const prisma = new PrismaClient();
  *  id: number
  * }
  */
-export async function createTodo(userId: number, title: string, description: string) {
-    
+export async function createTodo(
+  userId: number,
+  title: string,
+  description: string
+) {
+  const res = await prisma.todo.create({
+    data: {
+      userId,
+      title,
+      description,
+    },
+    select: {
+      userId: true,
+      title: true,
+      done: true,
+      description: true,
+      id: true,
+    },
+  });
+  //console.log(res);
+  return res;
 }
 /*
  * mark done as true for this specific todo.
@@ -26,7 +45,18 @@ export async function createTodo(userId: number, title: string, description: str
  * }
  */
 export async function updateTodo(todoId: number) {
-
+  const res = await prisma.todo.update({
+    where: { id: todoId },
+    data: { done: true },
+    select: {
+      title: true,
+      description: true,
+      id: true,
+      done: true,
+    },
+  });
+  //console.log(res);
+  return res;
 }
 
 /*
@@ -40,5 +70,11 @@ export async function updateTodo(todoId: number) {
  * }]
  */
 export async function getTodos(userId: number) {
-
+  const res = await prisma.todo.findMany({
+    where: { userId: userId },
+    select: {
+      userId: true,
+    },
+  });
+  return res;
 }
--- week-10/2-prisma-simple/src/db/user.ts
+++ week-10/2-prisma-simple/src/db/user.ts
@@ -1,7 +1,6 @@
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
-
 
 /*
  * Should insert into the users table
@@ -12,8 +11,26 @@ const prisma = new PrismaClient();
  *   name: string
  * }
  */
-export async function createUser(username: string, password: string, name: string) {
-    
+export async function createUser(
+  username: string,
+  password: string,
+  name: string
+) {
+  const res = await prisma.user.create({
+    data: {
+      username,
+      password,
+      name,
+    },
+    select: {
+      username: true,
+      password: true,
+      name: true,
+      id: true,
+    },
+  });
+  // console.log(res);
+  return res;
 }
 
 /*
@@ -25,5 +42,14 @@ export async function createUser(username: string, password: string, name: strin
  * }
  */
 export async function getUser(userId: number) {
-    
+  const res = await prisma.user.findUnique({
+    where: { id: userId },
+    select: {
+      username: true,
+      password: true,
+      name: true,
+      id: true,
+    },
+  });
+  return res;
 }
